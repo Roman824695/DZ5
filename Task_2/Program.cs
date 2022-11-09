664862afad605e4956638f587bdb768f904ec28c
Roman Tsykalo <u15921@mail.ru>
2022-11-10 03:34:28 +0400

    ДЗ выполнено

---
+++ Task_2/Program.cs
@@ -0,0 +1,31 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message + "-> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numbers = ReadInt("Введите число");
+int numbers2 = numbers;
+int sum = 0;
+int num = 1; 
+
+for(; numbers > 10; num++ )
+{
+    numbers /= 10;
+}
+
+int[] nums = new int[num];
+for(int i = 0; i < num; i++)
+{
+    int sam = numbers2 %10;
+    nums[i] = sam;
+    numbers2 /= 10;
+}
+
+for(int i = 0; i < num; i++ )
+{
+    sum += nums[i];
+}
+
+
+Console.WriteLine("Сумма цифр в числе -> " + sum);
---
+++ Task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message + "-> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int number = ReadInt("Сколько элементов вы хотите ввести?");
+
+int[] myArray = new int[number];
+
+for(int i = 0; i < number; i++)
+{
+   myArray[i] = ReadInt("Введите число"); 
+}
+
+Console.Write("[ " + myArray[0] + ", ");
+for(int i = 1; i < myArray.Length - 1; i++)
+{
+    Console.Write(myArray[i] + ", ");
+}
+Console.Write(myArray[myArray.Length - 1] + " ]");
